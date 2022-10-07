ca58f5457ae35fbc4b3cb00d45aa4e4129d43c75
JinKazama232 <r89mobile@gmail.com>
2022-10-07 09:08:22 -0500

    Add config file

---
+++ config.py
@@ -0,0 +1,14 @@
+#############################################################################################
+#// Path to folders containing media // #
+media_folders = (r'H:\\Movies', 
+                r'G:\\Movies', 
+                r'G:\\xSeed\\PTP')
+
+#// Blu API Token // #
+token = "fCYiRFERR0IhtD3wwvQIJRov8IlfjdEi5lU8CRID4NH1JM2IAmVsd9DW8PZXdhckruK6GnK9qCVu3TvPq39ihkq1CRElMU4bUaSZ"
+
+#// Torrent file output folder // #
+output_folder = r'D:\\Code\\Torrent Output\\Blu-Cross-Seed\\.torrents'
+
+#// Excel file report folder // #
+report_folder = r'D:\\Code\\Torrent Output\\Blu-Cross-Seed\\.excels'
